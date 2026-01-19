1722caba68a5de14eb7235bcd9d3955dd176f7d1
NEFT-Choi-Su <tnfuf7188@neft.pics>
2026-01-19 08:06:57 +0000

    feature(fe-test):테스트페이지생성

--- routes/web.php
+++ routes/web.php
@@ -11,59 +11,13 @@ use Inertia\Inertia;
 Route::fallback(function () {
     return redirect('home'); 
 });
+
 Route::get('/', function () {
     return redirect('/shop/home');
 })->name('/');
-Route::post('test',function(Request $request){
-     try{
-            // $data = $request->all();
-            $client = new Client();
-            $response = $client->post('http://3.35.79.133:5000/api', [
-                'json' => [
-                    'role' => 'auth',
-                    'action' => 'login',
-                    'payload' => [
-                        "email"=> "user@example11.com",
-                        "password" => "1234",
-                        // "name"=> "홍길동",
-                        // "phone"=> "010-0000-0000"
-                    ]
-                ],
-                'timeout' => 6000,
-                'connect_timeout' => 1000,
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'accept' => 'application/json',
-                ],
-            ]);
-            // $response = Http::post('http://3.35.79.133:5000/api', [
-            //     'role' => 'auth',
-            //     'action' => 'register',
-            //     'payload' => [
-            //         "email"=> "user@example.com",
-            //         "password" => "1234",
-            //         "name"=> "홍길동",
-            //         "phone"=> "010-0000-0000"
-            //     ]
-            // ]);
 
-            $result = json_decode($response->getBody(), true);
-            // Log::info($user);
-            Log::info($result);
-            // Log::info($request->judgment_text);
-            
-            return response()->json([
-                'status' => 'success',
-                'data' => $result,
-            ]);
-
-        }catch(\GuzzleHttp\Exception\ClientException $e){
-
-            return response()->json([
-                'status' => 'fail',
-                'message' => '서버에서 에러가 발생했습니다. 다시 시도해주세요'
-            ]);
-        }
+Route::get('test',function(Request $request){
+     return Inertia::render('test/index');
 })->name('test');
 
 Route::get('home', function () {

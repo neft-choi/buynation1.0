91b1619818bf9580d988185f5049ca65764e82a5
NEFT-Choi-Su <tnfuf7188@neft.pics>
2026-01-20 05:58:55 +0000

    feat(be-cart) : 카트기능개발

--- routes/shop/cart.php
+++ routes/shop/cart.php
@@ -1,4 +1,6 @@
 <?php 
     use App\Http\Controllers\Shop\Cart\CartController;
     use Illuminate\Support\Facades\Route;
+    Route::delete('cart/bulk', [CartController::class, 'bulkDestroy'])
+    ->name('cart.bulk-destroy');
     Route::resource('cart', CartController::class);

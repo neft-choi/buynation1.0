b2847b2a2764006bc0a5c7162ef4a81c33dca470
NEFT-Choi-Su <tnfuf7188@neft.pics>
2026-01-21 06:51:14 +0000

    feat(be-mypage/latest): 최근본 상품

--- app/Http/Controllers/Shop/MyPage/LatestController.php
+++ app/Http/Controllers/Shop/MyPage/LatestController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Shop\MyPage;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Shop\Mypage\LatestResource;
+use App\Services\ApiServer;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class LatestController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(ApiServer $apiService)
     {
-        //
+        $latestData = $apiService->send('shop','list_recent_products');
+        // dd($latestData);
+        return Inertia::render('shop/mypage/latest/index',[
+            'latestData' => LatestResource::collection($latestData['data']['items'])
+        ]);
     }
 
     /**
--- resources/js/pages/shop/mypage/latest/index.tsx
+++ resources/js/pages/shop/mypage/latest/index.tsx
@@ -1,71 +1,79 @@
 
-import { ShopLatestItemProps } from '@/components/shop/shop-latest-item'
-import ShopLatestItemContainer from '@/components/shop/shop-latest-item-container'
+import { ShopIconButton } from '@/components/shop/shop-button'
+import { ShopEmpty } from '@/components/shop/shop-empty'
+import { ShopIcon } from '@/components/shop/shop-icon'
+import { handleAddCart, ProductComponent } from '@/components/shop/shop-product-item'
 import ShopMypageLayout from '@/layouts/shop/mypage/shop-mypage-layout'
-import React from 'react'
-export const latestItems: ShopLatestItemProps[] = [
-    {
-        title: '[김정환홍삼] 홍삼으로 에너지타임 플러스',
-        img: 'https://placehold.co/600x400',
-        price: 249000,
-        url: 'test',
-        time: '2025.11.10',
-        discountRate: 75
-    },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스1',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.10',
+import { formatDiscountedKRW, formatKRW } from '@/lib/utils'
+import { Product } from '@/types/shop/public'
+import { Link } from '@inertiajs/react'
 
-    // },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스2',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.10',
+interface LatestData {
+    data: Product[]
+}
 
-    // },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스2',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.10',
-
-    // },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스2',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.10',
-
-    // },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스2',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.10',
-
-    // },
-    // {
-    //     title: '[김정환홍삼] 홍삼으로 에너지타임 플러스2',
-    //     img: 'https://placehold.co/600x400',
-    //     price: 249000,
-    //     url: 'test',
-    //     time: '2025.11.9',
-
-    // }
-]
-export default function Index() {
+function ShopLatestItem({ title, price, imageUrl, viewed_at, id }: Product) {
+    return (
+        <Link href={route('shop.product.show', { product: id })} className="w-full grid grid-cols-4 gap-4">
+            <div className="sr-only">{viewed_at}</div>
+            <div className="">
+                <img src={imageUrl[0] ?? 'https://placehold.co/100x200'} className="aspect-[8/10] rounded-sm object-cover" alt="" />
+            </div>
+            <div className="col-span-2 flex flex-col justify-center items-start">
+                <div className="text-sm">{title}</div>
+                <div className="line-through text-label-normal-subtler font-light text-sm">{price.price && formatKRW(price.price)}</div>
+                <div className="text-base  font-bold">
+                    {price.discountRate && <span className="text-status-negative mr-1">{price.discountRate}%</span>}
+                    <span className="">
+                        {price.sale && formatKRW(price.sale)}
+                    </span>
+                </div>
+            </div>
+            <div className="place-self-center justify-self-end">
+                <ShopIconButton size={'rectangleSm'} className="w-auto" onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault()
+                    handleAddCart(id, 1)
+                }}>
+                    <ShopIcon name='장바구니' />
+                </ShopIconButton>
+            </div>
+        </Link>
+    )
+}
+export default function Index({ latestData }: { latestData: LatestData }) {
+    const groupedItems = latestData.data && latestData.data.length > 0
+        ? latestData.data.reduce((acc, item) => {
+            const date = item.viewed_at!.split("T")[0]
+            if (!acc[date]) acc[date] = []
+            acc[date].push(item)
+            return acc
+        }, {} as Record<string, Product[]>)
+        : {}
+    const sortedDates = Object.keys(groupedItems).sort((a, b) => (a > b ? 1 : -1))
     return (
         <ShopMypageLayout title='최근 본 상품'>
             <div className='px-4 py-6  w-full'>
-                <ShopLatestItemContainer latestItems={latestItems} />
+                <div className="w-full text-label-solid-default">
+                    {sortedDates && sortedDates.length > 0 ? sortedDates.map((date) => (
+                        <div key={date} className="mb-6">
+                            <div className="w-full text-left font-semibold text-[15px] mb-3">
+                                {date}
+                            </div>
+                            <div className="w-full flex flex-col gap-3">
+                                {groupedItems[date].map((item) => (
+                                    <ShopLatestItem
+                                        key={item.title}
+                                        {...item}
+                                    />
+                                    // <ProductComponent type='4' key={item.id} {...item} />
+                                ))}
+                            </div>
+                        </div>
+                    )) : (
+                        <ShopEmpty icon='경고' text='최근 본 상품이 없습니다.' />
+                    )}
+                </div>
             </div>
         </ShopMypageLayout>
     )

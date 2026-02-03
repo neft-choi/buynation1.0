2d35bdb2d4cc4716afcfd50124f4c15cfdd4fe4c
NEFT-Choi-Su <tnfuf7188@neft.pics>
2026-02-03 01:46:53 +0000

    refactor(be-admin/seller) : [BE] 폴더구조 개혁 및 라우트 추가

--- routes/seller.php
+++ routes/seller.php
@@ -13,12 +13,44 @@ Route::middleware(['auth','verified','role_or_higher:'.RolesEnum::Seller->value]
         return Inertia::render('seller/index');
     })->name('index');
 
-    Route::get('products/create', function () {
-        return Inertia::render('seller/products/create');
-        })->name('create');
-    
-    Route::get('products/show', function () {
-        return Inertia::render('seller/products/show');
-        })->name('show');
+    //상품관리
+    Route::prefix('products')        
+    ->name('products.')
+    ->group(function(){
+        Route::get('product_1', function () {
+            return Inertia::render('seller/products/product_1/index');
+        })->name('product_1.index');
 
+        Route::get('product_2', function () {
+            return Inertia::render('seller/products/product_2/index');
+        })->name('product_2.index');
+    });
+
+    Route::prefix('orders')        
+    ->name('orders.')
+    ->group(function(){
+        Route::get('order_1', function () {
+            return Inertia::render('seller/orders/order_1/index');
+        })->name('order_1.index');
+
+        Route::get('order_2', function () {
+            return Inertia::render('seller/orders/order_2/index');
+        })->name('order_2.index');
+
+        Route::get('order_3', function () {
+            return Inertia::render('seller/orders/order_3/index');
+        })->name('order_3.index');
+
+        Route::get('order_4', function () {
+            return Inertia::render('seller/orders/order_4/index');
+        })->name('order_4.index');
+
+        Route::get('order_5', function () {
+            return Inertia::render('seller/orders/order_5/index');
+        })->name('order_5.index');
+
+        Route::get('order_6', function () {
+            return Inertia::render('seller/orders/order_6/index');
+        })->name('order_6.index');
+    });
 });
